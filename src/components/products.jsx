c67b28fc5beddae9105f149f7cdb8408f2a04195
cakajervisa <ervisa.cakaj@gmail.com>
2023-10-19 01:45:20 +0200

    Skeleton

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Products = () => {
+  // const[content,setContent] =useState(<ProductList/>);
   return (
     <div className='container my-5 text-center '>
-        <div className='mb-3'>Products</div>
+      <>
+        <ProductList />
+        <ProductForm />
+      </>
     </div>
-  )
+  );
 }
 
-export default Products
-;
+export default Products;
+
+function ProductList() {
+  
+  return (
+    <React.Fragment>
+      <h1 className='text-center mb-3'>List of Auctions</h1>
+      <button type="button" className='btn btn-primary m-2'>Create</button>
+    </React.Fragment>
+  );
+}
+
+function ProductForm() {
+  return (
+    <React.Fragment>
+      <h1 className='text-center mb-3'>Create a new Auction</h1>
+      <button type="button" className='btn btn-secondary m-2'>Cancel</button>
+    </React.Fragment>
+  );
+}
