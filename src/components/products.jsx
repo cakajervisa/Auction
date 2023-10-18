b0e91d8c9dc5ad65e6516b0c9164b0d10843135d
cakajervisa <ervisa.cakaj@gmail.com>
2023-10-19 01:53:55 +0200

    showList/showForm navigation

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 
 const Products = () => {
-  // const[content,setContent] =useState(<ProductList/>);
+  
+  const [content,setContent] =useState(<ProductList showForm={showForm}/>);
+  
+  function showList () {
+    setContent(<ProductList  showForm={showForm}/>);
+  }
+   function showForm () {
+    setContent(<ProductForm showList={showList}/>);
+  }
+  
   return (
     <div className='container my-5 text-center '>
       <>
-        <ProductList />
-        <ProductForm />
+        {content}
       </>
     </div>
   );
@@ -14,21 +22,20 @@ const Products = () => {
 
 export default Products;
 
-function ProductList() {
-  
+function ProductList(props) {
   return (
     <React.Fragment>
       <h1 className='text-center mb-3'>List of Auctions</h1>
-      <button type="button" className='btn btn-primary m-2'>Create</button>
+      <button onClick={()=>props.showForm()} type="button" className='btn btn-primary m-2'>Create</button>
     </React.Fragment>
   );
 }
 
-function ProductForm() {
+function ProductForm(props) {
   return (
     <React.Fragment>
       <h1 className='text-center mb-3'>Create a new Auction</h1>
-      <button type="button" className='btn btn-secondary m-2'>Cancel</button>
+      <button onClick={()=>props.showList()} type="button" className='btn btn-secondary m-2'>Cancel</button>
     </React.Fragment>
   );
 }
