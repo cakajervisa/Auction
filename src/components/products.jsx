2a6c083cef99e480951c42a19385afc22f0d091f
cakajervisa <ervisa.cakaj@gmail.com>
2023-10-19 02:37:36 +0200

    Fetching the data

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
   
@@ -23,19 +23,73 @@ const Products = () => {
 export default Products;
 
 function ProductList(props) {
+   const[products,setProducts] =useState([]);
+
+   function fetchProducts(){
+    
+    fetch("http://localhost:3001/products")
+  .then ((response) => {
+    if(!response.ok){
+      throw new Error ("Unexpected Server Error");
+    }
+    return response.json()
+  })
+  .then((data) => {
+    // console.log(data)
+    setProducts(data);
+  })
+  .catch((error) => console.log("err",error));
+   }
+
+useEffect(()=>fetchProducts(),[]);
+
+
   return (
-    <React.Fragment>
+    <>
       <h1 className='text-center mb-3'>List of Auctions</h1>
       <button onClick={()=>props.showForm()} type="button" className='btn btn-primary m-2'>Create</button>
-    </React.Fragment>
+   <table className='table'>
+<thead>
+  <tr>
+    <th>Product</th>
+    <th>Seller</th>
+    <th>Top Bid</th>
+    <th>Time Remaining</th>
+  </tr>
+</thead>
+<tbody>
+  {
+
+    products.map((product,index)=>{
+      return(
+        <tr key={index}>
+          <td>{product.prodctname}</td>
+           <td>{product.seller}</td>
+            <td>${product.topbid }</td>
+             <td>{product. timeremaning}</td>
+          
+          <td style={{ width: '10px', whiteSpace: 'nowrap' }}>
+
+
+            <button type='button' className='btn btn-primary btn-sm  me-2'>Bid</button>
+            <button type="button" className='btn btn-danger btn-sm'>Delete</button>
+          </td>
+        </tr>
+      )
+    })
+  }
+</tbody>
+   </table>
+   
+    </>
   );
 }
 
 function ProductForm(props) {
   return (
-    <React.Fragment>
+    <>
       <h1 className='text-center mb-3'>Create a new Auction</h1>
       <button onClick={()=>props.showList()} type="button" className='btn btn-secondary m-2'>Cancel</button>
-    </React.Fragment>
+    </>
   );
 }
